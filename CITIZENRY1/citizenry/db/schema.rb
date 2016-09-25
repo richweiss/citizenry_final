aaa840db12b6a6649fa10cc65e3942f0d203e0de
Rich Weiss <richweissnyc@gmail.com>
2016-09-25 00:28:07 -0400

    database rollback and destroy model comment

--- CITIZENRY1/citizenry/db/schema.rb
+++ CITIZENRY1/citizenry/db/schema.rb
@@ -10,13 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160925041149) do
-
-  create_table "comments", force: :cascade do |t|
-    t.string   "name"
-    t.text     "description"
-    t.datetime "created_at",  null: false
-    t.datetime "updated_at",  null: false
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
