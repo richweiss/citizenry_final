d8e526ed3a3dad992f7de4eaf5626fd91e524f57
Rich Weiss <richweissnyc@gmail.com>
2016-09-25 22:48:02 -0400

    created Comments controller

--- CITIZENRY3/citizenry3/config/routes.rb
+++ CITIZENRY3/citizenry3/config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
     root to: "articles#index"
-    resources :articles do
-      resources :comments
+     resources :articles do
+        resources :comments
   end
 end
 
