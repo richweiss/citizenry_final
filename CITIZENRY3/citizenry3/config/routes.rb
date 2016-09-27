cec92b6acb2987bf4b79a21542cfa1613cf2041a
Rich Weiss <richweissnyc@gmail.com>
2016-09-27 08:28:11 -0400

    devise authentification working need logout

--- CITIZENRY3/citizenry3/config/routes.rb
+++ CITIZENRY3/citizenry3/config/routes.rb
@@ -1,10 +1,20 @@
 Rails.application.routes.draw do
   devise_for :admins
   devise_for :users
-  root to: "articles#index"
+
+  devise_scope :user do
+  authenticated :user do
+    root 'articles#index', as: :authenticated_root
+  end
+
+  unauthenticated do
+    root 'devise/sessions#new', as: :unauthenticated_root
+  end
+end
+
      resources :articles do
         resources :comments
-  end
+end
 
   get "/search" => "articles#index"
 
