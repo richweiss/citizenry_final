6aef863a1c1062adba502f398105d35db258c349
Rich Weiss <richweissnyc@gmail.com>
2016-09-30 12:58:02 -0400

    About section updates

--- CITIZENRY3/citizenry3/app/controllers/articles_controller.rb
+++ CITIZENRY3/citizenry3/app/controllers/articles_controller.rb
@@ -26,13 +26,13 @@ class ArticlesController < ApplicationController
 
   def create
     @article = Article.new(article_params)
-
     if @article.save
     redirect_to @article
     else
       render 'new'
     end
   end
+
 
   def update
   @article = Article.find(params[:id])
@@ -53,7 +53,7 @@ class ArticlesController < ApplicationController
 
 private
   def article_params
-    params.require(:article).permit(:title, :text)
+    params.require(:article).permit(:title, :content)
   end
 
 end
--- CITIZENRY3/citizenry3/config/routes.rb
+++ CITIZENRY3/citizenry3/config/routes.rb
@@ -6,12 +6,10 @@ Rails.application.routes.draw do
   authenticated :user do
     root 'articles#index', as: :authenticated_root
   end
-
   unauthenticated do
     root 'devise/sessions#new', as: :unauthenticated_root
   end
 end
-
   devise_scope :user do
     get '/users/sign_out', to: 'devise/sessions#destroy', as: :signout
   end
